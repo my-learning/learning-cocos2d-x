90d06e5697462b7c96562dc6142ad63c9d88bd72
futoase <futoase@gmail.com>
2014-01-21 00:52:16 +0900

    [WIP] Created: Added CCPlaySE.

---
+++ Test01/Classes/GameSchene.cpp
@@ -0,0 +1,9 @@
+//
+//  GameSchene.cpp
+//  Test01
+//
+//  Created by 松崎 啓治 on 2014/01/21.
+//
+//
+
+#include "GameSchene.h"
---
+++ Test01/Classes/GameSchene.h
@@ -0,0 +1,14 @@
+//
+//  GameSchene.h
+//  Test01
+//
+//  Created by 松崎 啓治 on 2014/01/21.
+//
+//
+
+#ifndef Test01_GameSchene_h
+#define Test01_GameSchene_h
+
+
+
+#endif
